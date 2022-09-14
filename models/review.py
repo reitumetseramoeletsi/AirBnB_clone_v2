b64204db8269ed18b55c7811c737bbbaa6be5a0e
jacobnzala <63255452+jacobnzala@users.noreply.github.com>
2022-09-14 20:53:03 +0200

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """This is the review class"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from models.base_model import BaseModel, Base, Column, String
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
 
 
 class Review(BaseModel, Base):
-    """This is the class for Review
-    Attributes:
-        place_id: place id
-        user_id: user id
-        text: review description
-    """
-    __tablename__ = "reviews"
-    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    """This is the class for Review"""
+    __tablename__ = 'reviews'
+
     text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+
+    user = relationship('User', back_populates='reviews')
+    place = relationship('Place', back_populates='reviews')
